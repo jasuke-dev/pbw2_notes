cff8905d55d31f14ca4e7f0d59930a0158ba02e5
jasuke-dev <ilham.suryaega@gmail.com>
2022-05-07 15:54:03 +0700

    fix tampilan notes

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,6 +1,10 @@
 <?php $this->extend('layouts/home_layout')?>
 
 <?php $this->section('content') ?>
+    <div class="header">
+        <h1>Notes for Ilham</h1>
+        <button class="logout" type="button"><i class="fa-solid fa-right-from-bracket"></i></button>
+    </div>
     <div id="app">
         <?php foreach($notes as $note) : ?>
             <!-- <textarea class="note"><?= $note['content'] ?></textarea> -->
--- app/Views/layouts/home_layout.php
+++ app/Views/layouts/home_layout.php
@@ -15,6 +15,9 @@
     <!-- Custom fonts for this template-->
     <link href="<?php echo base_url(); ?>/css/style.css" rel="stylesheet" type="text/css">
     <script src="<?php echo base_url(); ?>/js/main.js" defer></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+        integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 <body>
